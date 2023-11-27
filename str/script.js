689592d9ef52483a8b8b094bc7b86337c2c74850
shukhratjon717 <shukhratjon0717@gmail.com>
2023-11-27 16:19:32 +0900

    new file added

--- str/script.js
+++ str/script.js
@@ -22,9 +22,14 @@ if ((year % 4 == 0) && (year % 100 == 0) || (year % 400 ==0))
 console.log("Leap year")
 else console.log("Not a leap year")
 
+
+//  replace method
 var text = "Webbrain "
 
-console.log(text.replace("Web" , "IT"))
+var web1 = text.replace("Web" , "IT")
+
+console.log( web1.repeat(10))
+
 
 
 
