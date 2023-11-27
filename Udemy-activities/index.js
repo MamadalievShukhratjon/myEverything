ee72b00dabf0e02832d34daeb264213c4996aef3
shukhratjon717 <shukhratjon0717@gmail.com>
2023-11-27 20:31:23 +0900

    New branch added

---
+++ Udemy-activities/index.js
@@ -0,0 +1,12 @@
+// working with arrays
+var names = ['shukhrat', 'ali', 'joey', 'jimmy', 'john']
+
+var guestName = prompt("What is your name? ")
+
+if (names.includes(guestName)) {
+    console.log("You are welcome")
+}
+else {
+    console.log("You are not welcome")
+}
+
