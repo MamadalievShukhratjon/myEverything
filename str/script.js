3f74e71d44b4818cf17841c0d05b7b0c53e1b291
shukhratjon717 <shukhratjon0717@gmail.com>
2023-11-27 15:35:01 +0900

    new file added

--- str/script.js
+++ str/script.js
@@ -4,3 +4,20 @@ let str = "school"
 
 console.log((str.endsWith("l")) || (str.endsWith("L")))
 // console.log(str.endsWith("L"))
+
+
+// reverse the strings
+var str1 = "Web"
+var str2 = "brain"
+var str3 = str1 
+str1 = str2 
+str2 = str3
+console.log(str1)
+
+// Leap year 
+
+var year = "2000"
+
+if ((year % 4 == 0) && (year % 100 == 0) || (year % 400 ==0))
+console.log("Leap year")
+else console.log("Not a leap year")
