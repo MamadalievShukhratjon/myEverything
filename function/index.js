7dd65f429c33ab4b6b26ffd20c94148cc7507590
shukhratjon717 <shukhratjon0717@gmail.com>
2023-11-30 10:44:59 +0900

    new file added and bug is fixed

---
+++ function/index.js
@@ -0,0 +1,11 @@
+for (let i = 0; i <= 10; i++ ) {
+    if (i % 0 === 0) {
+        console.log("Nomalum")
+    }else if (i % 2 === 0) {
+        console.log("Juft son")
+    }else {
+        console.log("toq son");
+    }
+
+    console.log(i)
+}
